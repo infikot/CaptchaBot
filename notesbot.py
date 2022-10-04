3746bec64ac877ef13863ec90256346e6b6db63c
INFIKot <92332233+infikot@users.noreply.github.com>
2022-10-04 20:08:14 +0300

    Add files via upload

--- notesbot.py
+++ notesbot.py
@@ -9,17 +9,23 @@ from vkbottle.bot import Bot
 from vkbottle.dispatch.rules.base import CommandRule
 from vkbottle.user import Message
 
-from config import tokenbot, tokenalisia
 from rule import Permission
 
+""""
 logger.remove()
 logger.add(sys.stderr, level="INFO")
+"""
+
+tokenbot = input("Введите токен бота: \n")
+tokenalisia = input("Введите токен юзербота: \n")
 
 bot = Bot(token=tokenbot)
-userbot = User(token=tokenuser)
+userbot = User(token=tokenalisia)
+
 
 bot.labeler.custom_rules["Permission"] = Permission
 
+imagefurrynocopy = "0"
 
 @bot.on.message(text="/ping")
 async def info_hand(message: Message):
@@ -29,13 +35,13 @@ async def info_hand(message: Message):
 @bot.on.message(text="/ac")
 async def info_hand(message: Message):
     await message.answer(
-        "Беседы и их команды: \n \n https://vk.me/join/p77tSVT2BOvT9d0_xDIQ5tG4l0R0pw2moIA= \n Заметки, ранг <18+> \n Разрешены /r* команды: \n Все. \n \n https://vk.me/join/mMYQJA/I3b/Q0STlMZHjbTkCC4/xaUqJsZU= \n Заметки, ранг <offtop> \n Разрешены /r* команды: \n /rlegs; /random; /rmems; \n \n https://vk.me/join/QOeVTQqLOAFIv2TzwBd0_A/IlCCWDn65fjs= \n Заметки, ранг <default> \n Разрешены /r* команды: \n Нет разрешённых команд /r*")
+        "Беседы и их команды: \n \n none information")
 
 
 @bot.on.chat_message(text="/info")
 async def info_hand(message: Message):
     await message.answer(
-        "Информация: \n Мой создатель - vk.com/all.s.ninehr \n Основные команды добавлены 06.08.2022 \n На данный момент добавлено 30% от нужных функций, но в скоре все 100% будут добавлены в меня//~ \n Отдельная благодарность: \n vk.com/id583976210,  vk.com/id715892900,  vk.com/id601395326")
+        "Информация: \n Мой создатель - vk.com/all.s.ninehr \n Основные команды добавлены 06.08.2022 \n На данный момент добавлено 50% от нужных функций, но в скоре все 100% будут добавлены в меня//~ \n Отдельная благодарность: \n vk.com/id583976210,  vk.com/id715892900,  vk.com/id601395326")
 
 
 @bot.on.chat_message(text="/help")
@@ -45,15 +51,10 @@ async def help_hand(message: Message):
         f"Приветик {user[0].first_name}! Я - бот Алисия! 😸 \n Я очень люблю Заметки, по этому я и создана! \n Вот что я могу: \n /random;   /rhentai; \n /rxxx;   /rlegs; \n /info;   /ac; \n /rmems;   /rhorny; \n /ac;   /help;")
 
 
-@bot.on.chat_message(Permission(["638920116", "1"]), text="/kick")
-async def kick(message: Message):
-    await bot.api.messages.remove_chat_user(message.peer_id - 2000000000, message.reply_message.from_id)
-    return "Пользователь успешно исключён из данной беседы"
-
 
 @bot.on.chat_message(text="/rlegs")
 async def rlegs(message: Message):
-    if message.chat_id in (2, 3, 4):
+    if message.chat_id in (2, 5, 4):
         photos = await userbot.api.photos.get(owner_id=-214905751, album_id="285924951")
         photo = random.choice(photos.items)
         await message.answer(message="Вот тебе ножки, семпай~", attachment=f"photo{photo.owner_id}_{photo.id}")
@@ -65,7 +66,7 @@ async def rlegs(message: Message):
 
 @bot.on.chat_message(text="/rxxx")
 async def rxxx(message: Message):
-    if message.chat_id == 3:
+    if message.chat_id == 5:
         photos = await userbot.api.photos.get(owner_id=-214905751, album_id="285930017")
         photo = random.choice(photos.items)
         await message.answer(
@@ -79,11 +80,37 @@ async def rxxx(message: Message):
 
 @bot.on.chat_message(text="/rmems")
 async def rmems(message: Message):
-    if message.chat_id in (2, 3, 4):
+    if message.chat_id in (2, 4, 5):
         photos = await userbot.api.photos.get(owner_id=-214905751, album_id="285930013")
         photo = random.choice(photos.items)
         await message.answer(message="Ого, шуточки? Хехе, хорошо, развлекайся, семпай!~",
                              attachment=f"photo{photo.owner_id}_{photo.id}")
+        photo = 0
+    else:
+        await message.answer(
+            "В данной беседе эта команда запрещена. \n Проверь полный список команд и их разрешений с помощью команды /ac")
+
+@bot.on.chat_message(text="/rhorny")
+async def rhorny(message: Message):
+    if message.chat_id in (5, 4):
+        print("\n \n \n" + str(imagefurrynocopy) + "\n \n \n")
+        photos = await userbot.api.photos.get(owner_id=-214905751, album_id="285930010")
+        photo = random.choice(photos.items)
+        if photo.id in imagefurrynocopy:
+            photo = random.choice(photos.items)
+            if photo.id in imagefurrynocopy:
+                photo = random.choice(photos.items)
+                await message.answer(message="Решил пошалить со своими зверюшками? Ну, удачи, семпай!~",
+                                     attachment=f"photo{photo.owner_id}_{photo.id}")
+                imagefurrynocopy.append(photo.id)
+            else:
+                await message.answer(message="Решил пошалить со своими зверюшками? Ну, удачи, семпай!~",
+                                 attachment=f"photo{photo.owner_id}_{photo.id}")
+                imagefurrynocopy.append(photo.id)
+        else:
+            await message.answer(message="Решил пошалить со своими зверюшками? Ну, удачи, семпай!~",
+                                 attachment=f"photo{photo.owner_id}_{photo.id}")
+            imagefurrynocopy.append(photo.id)
         photo = 0
     else:
         await message.answer(
@@ -97,32 +124,10 @@ async def chat_id(message: Message):
 
 @bot.on.chat_message(text="/test")
 async def test(message: Message):
-    if message.chat_id in (3, 4):
+    if message.chat_id in (3, 4, 5):
         await message.answer("This chat avalible")
     else:
         await message.answer("This chat do not avalible")
-
-
-
-@bot.on.chat_message(CommandRule("say", ["/"], 1))
-async def say_handler(message: Message, args: Tuple[str]):
-    cmids = message.conversation_message_id
-    await bot.api.messages.delete(peer_id=message.peer_id, delete_for_all=1, cmids=cmids)
-    return bot.on.chat_message(f"<<{args[0]}>>")
-
-
-
-@bot.on.chat_message(text="/random <first:int> <second:int>")
-async def say_handler(message: Message, first, second):
-    rand = random.randint(first, second)
-    print(rand)
-    await message.answer(f"Рандомная цифра равна: {rand}")
-
-'''
-@bot.on.chat_message(Permission(), text="/testrule")
-async def testrule(message: Message):
-    await message.answer("It's work!")
-'''
 
 
 bot.run_forever()
